953563b09672af3a5031b0f0b22de8013e303516
Shuyibe <shuyibesiddikif@gmail.com>
2025-01-14 12:35:53 +0600

    commit

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // parser
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://www.immogroup.ro', credentials: true }));
 app.use(cookieParser());
 
 // application routes
